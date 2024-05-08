a4e65dc918746f0c1764d6348f9fe23d7795608e
Aparecido Celso de Souza Junior <celsojrbr@gmail.com>
2024-05-08 16:04:14 -0300

    subindo a versão 3 do desafio calculo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Jogador =
+let Jogador1 =
 {
     NomeJogador:"",
     NivelJogador:"",
@@ -32,7 +32,8 @@ function CalcularRankeadas(JogadorObj){
     SetNivelJogador(QuantidadeRankeadas);
     //O Herói tem de saldo de {saldoVitorias} está no nível de {nivel}
     //return "Seu rank "+GetRankeadaJogador(JogadorObj)+", Vitorias: "+GetQtsVitoria(JogadorObj) +" Derrotas: "+GetQtsDerrota(JogadorObj);
-    return "O Herói tem de saldo de " + GetQtsVitoria(JogadorObj) + " está no nível de " + GetRankeadaJogador(JogadorObj);
+    //return ;
+    console.log("O Herói "+GetNomeJogador(JogadorObj)+" tem de saldo de " + GetQtsVitoria(JogadorObj) + " está no nível de " + GetRankeadaJogador(JogadorObj));
 }
 
 
@@ -80,22 +81,77 @@ function GetQtsVitoria(JogadorObj){
 }
 
 // função que adiciona ao array do jogador
-function ADDPartida(JogadorObj,Resultado){
-    JogadorObj.Partidas.push(Resultado);
-    if(Resultado=="vitória"){
-        SetQtsVitoria(JogadorObj,GetQtsVitoria(JogadorObj)+1);
-    }else{
-        SetQtsDerrota(JogadorObj,GetQtsDerrota(JogadorObj)+1);
+function ADDPartida(JogadorObj,Resultado,NumerodePartidas){
+    for(index=0; index<=NumerodePartidas; index++ ){
+        JogadorObj.Partidas.push(Resultado);
+        if(Resultado=="vitória"){
+            SetQtsVitoria(JogadorObj,GetQtsVitoria(JogadorObj)+1);
+        }else{
+            SetQtsDerrota(JogadorObj,GetQtsDerrota(JogadorObj)+1);
+        }
     }
 };
 
 
-// for para colocar 150 vitorias para o jogador
-for(index=0; index<=150; index++ ){
-    ADDPartida(Jogador,"vitória");
+// for para colocar 150 vitorias para o jogador1
+
+ADDPartida(Jogador1,"vitória",150);
+
+
+SetNomeJogador(Jogador1,"Aparecido Celso de Souza Junior");
+
+//console.log(CalcularRankeadas(Jogador1));
+
+
+
+// for para colocar 80 vitorias para o jogador1
+let Jogador2 =
+{
+    NomeJogador:"",
+    NivelJogador:"",
+    RankeadaJogador:"",
+    Partidas:[],
+    QtsVitoria:0,
+    QtsDerrota:0, 
+}
+SetNomeJogador(Jogador2,"Kleber Silva de Souza");
+
+ADDPartida(Jogador2,"vitória",80);
+
+//console.log(CalcularRankeadas(Jogador2));
+
+
+
+// for para colocar 50 vitorias para o jogador1
+let Jogador3 =
+{
+    NomeJogador:"",
+    NivelJogador:"",
+    RankeadaJogador:"",
+    Partidas:[],
+    QtsVitoria:0,
+    QtsDerrota:0, 
 }
 
-SetNomeJogador("Aparecido Celso de Souza Junior");
+ADDPartida(Jogador3,"vitória",50);
 
-console.log(CalcularRankeadas(Jogador));
+SetNomeJogador(Jogador3,"Karina Silva de Souza");
+//console.log(CalcularRankeadas(Jogador3));
 
+console.log("Bem vindo a Calculadora de partidas Rankeadas, aqui nesse desafio foi feito 3 objetos de jogador");
+console.log("Jogador1,Jogador2,Jogador3");
+console.log("dentro desse objeto temos: NomeJogador, NivelJogador, RankeadaJogador, Partidas, QtsVitoria, QtsDerrota");
+console.log("NomeJogador, nome definido para o jogador,");
+console.log("NivelJogador, qual o nivel atual do jogador,");
+console.log("RankeadaJogador, qual rank jogador esta atualmente");
+console.log("QtsVitoria, quantidade de vitorias que o jogador tem");
+console.log("QtsDerrota, quantidade de derrotas que o jogador tem");
+
+console.log("Partidas, historioco das jogadas em formato ARRAY pode adicionar partidas para o jogador usando a função ADDPartida");
+console.log("para usar a função ADDPartida deve passar 3 parametros, o jogador que deseja adicionar a partida, o resultado da partida, e quantidade que deseja adicionar exemplo:");
+console.log("ADDPartida(Jogador1,\"vitória\",1); nesse exemplo vai adicionar 1 vitoria para o jogador1");
+console.log("ADDPartida(Jogador1,\"Derrota\",10); nesse caso vai adicionar 10 derrotas ao jogador1");
+
+console.log("pode usar a função CalcularRankeadas(Jogador1), CalcularRankeadas(Jogador2), CalcularRankeadas(Jogador3) para visualizar os resultados dos ranks de cada jogador exemplo:");
+console.log("CalcularRankeadas(Jogador1); nesse caso vai mostrar \"O Herói Aparecido Celso de Souza Junior tem de saldo de 151 está no nível de Imortal\" ");
+CalcularRankeadas(Jogador1);
